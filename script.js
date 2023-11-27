1623ef626aabacc6a843bd522e9ac7a8405e5d80
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-11-27 23:22:33 +0400

    Setting Up Our Development Environment

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Why Do Front-End Frameworks Exist?
 // React vs. Vanilla JavaScript
 // What is React?
+// Setting Up Our Development Environment
+
+const flickSwitch = function (arr, result = true) {
+  return arr.map((word, i) => {
+    if (word === "flick") result = !result;
+    return result;
+  });
+};
+
+console.log(flickSwitch(["bicycle", "jarmony", "flick", "sheep", "flick"]));
+
+let a = 1;
+let b = 2;
+a = a + 1;
+
+console.log(a, b);
